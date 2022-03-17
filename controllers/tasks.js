f20170df0979f6a573adc87ff3a3b503d8087dd5
ayushgupta0010 <ayushgupta0010@gmail.com>
2022-03-17 15:22:26 +0530

    Final commit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,37 +1,50 @@
-const Task = require("../model/Task");
+const Task = require("../models/Task");
+const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/custom-error");
 
-const getAllTasks = (req, res) => {
-  Task.find({})
-    .then((tasks) => res.status(200).json(tasks))
-    .catch((err) => res.status(400).json(err));
-};
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+});
 
-const createTask = (req, res) => {
-  Task.create(req.body)
-    .then((task) => res.status(201).json(task))
-    .catch((err) => res.status(400).json(err));
-};
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
 
-const getTask = (req, res) => {
-  Task.findById(req.params.id)
-    .then((task) => res.status(200).json(task))
-    .catch((err) => res.status(404).json(err));
-};
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
+  }
 
-const updateTask = (req, res) => {
-  Task.findByIdAndUpdate(req.params.id, req.body, {
+  res.status(200).json({ task });
+});
+
+const deleteTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
+  }
+  res.status(200).json({ task });
+});
+
+const updateTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
     new: true,
     runValidators: true,
-  })
-    .then((task) => res.status(200).json(task))
-    .catch((err) => res.status(400).json(err));
-};
+  });
 
-const deleteTask = (req, res) => {
-  Task.findByIdAndDelete(req.params.id)
-    .then((task) => res.status(200).json(task))
-    .catch((err) => res.status(400).json(err));
-};
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
+  }
+
+  res.status(200).json({ task });
+});
 
 module.exports = {
   getAllTasks,
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const connectDb = require("./db/connect");
 const taskRoutes = require("./routes/tasks");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 const app = express();
 const port = 3000;
@@ -12,11 +14,10 @@ app.use(cors());
 app.use(express.static("./public"));
 app.use(express.json());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.use("/api/v1/tasks", taskRoutes);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 connectDb(process.env.MONGODB_URL)
   .then(() => {
