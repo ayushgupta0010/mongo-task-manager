6c5a6fdbc612003fd6434662cf1f0d59235d089a
ayushgupta0010 <ayushgupta0010@gmail.com>
2022-03-17 13:46:42 +0530

    My version of performing CRUD operations

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,36 @@
+const Task = require("../model/Task");
+
 const getAllTasks = (req, res) => {
-  res.send("all items");
+  Task.find({})
+    .then((tasks) => res.status(200).json(tasks))
+    .catch((err) => res.status(400).json(err));
 };
 
 const createTask = (req, res) => {
-  res.json(req.body);
+  Task.create(req.body)
+    .then((task) => res.status(201).json(task))
+    .catch((err) => res.status(400).json(err));
 };
 
 const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+  Task.findById(req.params.id)
+    .then((task) => res.status(200).json(task))
+    .catch((err) => res.status(404).json(err));
 };
 
 const updateTask = (req, res) => {
-  res.send("update task");
+  Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+    .then((task) => res.status(200).json(task))
+    .catch((err) => res.status(400).json(err));
 };
 
 const deleteTask = (req, res) => {
-  res.send("delete task");
+  Task.findByIdAndDelete(req.params.id)
+    .then((task) => res.status(200).json(task))
+    .catch((err) => res.status(400).json(err));
 };
 
 module.exports = {
